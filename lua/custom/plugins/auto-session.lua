ed3a9e54b9629b215ecd1a3f64a4ce60ab4542fd
long9657 <longlehien9657@gmail.com>
2026-03-20 09:04:53 +0700

    refactor: auto-session

--- lua/custom/plugins/auto-session.lua
+++ lua/custom/plugins/auto-session.lua
@@ -1,7 +1,7 @@
 return {
   'rmagatti/auto-session',
   lazy = false,
-
+  -- enabled = false,
   ---enables autocomplete for opts
   ---@module "auto-session"
   ---@type AutoSession.Config
@@ -10,7 +10,6 @@ return {
     { '<leader>wr', '<cmd>AutoSession search<CR>', desc = 'Session search' },
     { '<leader>ws', '<cmd>AutoSession save<CR>', desc = 'Save session' },
     { '<leader>wa', '<cmd>AutoSession toggle<CR>', desc = 'Toggle autosave' },
-    { '<leader>wd', '<cmd>AutoSession deletePicker<CR>', desc = 'DeletePicker autosave' },
   },
   opts = {
     (function() vim.o.sessionoptions = 'blank,buffers,curdir,folds,help,tabpages,winsize,winpos,terminal,localoptions' end)(),
@@ -25,76 +24,18 @@ return {
       'noice',
       'notify',
     },
-    -- post_save_cmds = {
-    --   function()
-    --     local cwd = vim.fn.getcwd()
-    --     if not cwd or cwd == '' then
-    --       return
-    --     end
-    --     local root = vim.loop.fs_realpath(cwd) or cwd
-    --
-    --     for _, buf in ipairs(vim.api.nvim_list_bufs()) do
-    --       if vim.api.nvim_buf_is_loaded(buf) and vim.bo[buf].buflisted then
-    --         local name = vim.api.nvim_buf_get_name(buf)
-    --         if name ~= '' then
-    --           local path = vim.loop.fs_realpath(name) or name
-    --           if not path:lower():find(root:lower(), 1, true) then
-    --             if #vim.fn.win_findbuf(buf) == 0 then
-    --               vim.api.nvim_buf_delete(buf, { force = false })
-    --             end
-    --           end
-    --         end
-    --       end
-    --     end
-    --   end,
-    -- },
-    --
-    --linux
-    --
-    -- pre_save_cmds = {
-    --   function()
-    --     local cwd = vim.uv.cwd()
-    --     local bufs = vim.api.nvim_list_bufs()
-    --
-    --     if not cwd then
-    --       return
-    --     end
-    --     for _, buf in ipairs(bufs) do
-    --       if vim.api.nvim_buf_is_loaded(buf) then
-    --         local name = vim.api.nvim_buf_get_name(buf)
-    --         if name ~= '' and not name:find(cwd, 1, true) then
-    --           -- buffer has a name and is not under cwd, close it
-    --           vim.api.nvim_buf_delete(buf, { force = true })
-    --         end
-    --       end
-    --     end
-    --   end,
-    -- },
-    --
-    -- windows
-    --
     pre_save_cmds = {
       function()
         local cwd = vim.uv.cwd()
         local bufs = vim.api.nvim_list_bufs()
 
-        if not cwd then
-          return
-        end
-
-        -- normalize cwd for Windows (slashes + lowercase)
-        cwd = cwd:gsub('\\', '/'):lower()
-
+        if not cwd then return end
         for _, buf in ipairs(bufs) do
           if vim.api.nvim_buf_is_loaded(buf) then
             local name = vim.api.nvim_buf_get_name(buf)
-            if name ~= '' then
-              -- normalize buffer path too
-              local norm_name = name:gsub('\\', '/'):lower()
-              if not norm_name:find(cwd, 1, true) then
-                -- buffer has a name and is not under cwd, close it
-                vim.api.nvim_buf_delete(buf, { force = true })
-              end
+            if name ~= '' and not name:find(cwd, 1, true) then
+              -- buffer has a name and is not under cwd, close it
+              vim.api.nvim_buf_delete(buf, { force = true })
             end
           end
         end
